63d81fe2e6203596586364858c8aba14391fc214
Ashiquetkorambayil <102928710+Ashiquetkorambayil@users.noreply.github.com>
2024-08-19 10:05:30 +0530

    18

--- src/firebase/messaging.js
+++
@@ -1,8 +0,0 @@
-// src/firebase/messaging.js
-import firebase from '../config/config';
-
-const messaging = firebase.messaging();
-
-export const requestPermission = () => messaging.requestPermission();
-export const onMessage = (callback) => messaging.onMessage(callback);
-export const onTokenRefresh = (callback) => messaging.onTokenRefresh(callback);
